cdc6c54120b3157ff6e78749953ee963028446a1
Jadalontoria <jadalontoriaa@gmail.com>
2023-07-27 14:32:47 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Person from './components/profile/Person'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Person fullName='Maria Etuk' bio=' I build scalable web applications.
+ ' profession='Full-Stack Software Developer'/>
+    </div>
+  )
+}
+
+export default App
